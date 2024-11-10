d8705cc999f1f91e7d55c05b08445e45a190d2cd
Oscarmacieira <macieiraoscar@icloud.com>
2024-11-10 03:39:16 +0000

    refactor(icons): svg props

--- src/components/icons/CheckIcon.tsx
+++ src/components/icons/CheckIcon.tsx
@@ -3,16 +3,17 @@ import { SVGProps } from "react";
 export const CheckIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={40}
-    height={40}
+    width="40"
+    height="40"
     fill="none"
+    viewBox="0 0 40 40"
     {...props}
   >
     <path
       fill="#39A699"
       fillRule="evenodd"
-      d="M20 36c8.837 0 16-7.163 16-16S28.837 4 20 4 4 11.163 4 20s7.163 16 16 16Zm7.713-19.618a1.5 1.5 0 0 0-2.426-1.764l-6.968 9.58-3.758-3.759a1.5 1.5 0 0 0-2.122 2.122l5 5a1.5 1.5 0 0 0 2.274-.179l8-11Z"
+      d="M20 36c8.837 0 16-7.163 16-16S28.837 4 20 4 4 11.163 4 20s7.163 16 16 16m7.713-19.618a1.5 1.5 0 0 0-2.426-1.764l-6.968 9.58-3.758-3.759a1.5 1.5 0 0 0-2.122 2.122l5 5a1.5 1.5 0 0 0 2.274-.179z"
       clipRule="evenodd"
-    />
+    ></path>
   </svg>
 );
