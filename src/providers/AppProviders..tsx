3a26d8826b1a78705427c2787657ee6185abf03c
Oscarmacieira <macieiraoscar@icloud.com>
2024-11-11 17:34:01 +0000

    feat(providers): added global app provider

---
+++ src/providers/AppProviders..tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { AuthProvider } from "@/features/auth/providers/AuthProvider";
+import { TransactionManagerProvider } from "@/features/transactions/providers/TransactionManagerProvider";
+import { VaultProvider } from "@/features/vaults/providers/VaultProvider";
+import { initialChain, rainbowConfig } from "@/lib/rainbow-config";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+
+const client = new QueryClient();
+
+export const AppProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={rainbowConfig}>
+      <QueryClientProvider client={client}>
+        <RainbowKitProvider initialChain={initialChain}>
+          <AuthProvider>
+            <TransactionManagerProvider>
+              <VaultProvider>{children}</VaultProvider>
+            </TransactionManagerProvider>
+          </AuthProvider>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
