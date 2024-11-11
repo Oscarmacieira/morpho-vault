5f7c77fa3d39548cdbcbc0edf706c9f11bb04a8c
Oscarmacieira <macieiraoscar@icloud.com>
2024-11-11 17:34:53 +0000

    feat(app): added main layout and page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.scss";
 import { siteConfig } from "@/lib/site-config";
+import { AppProviders } from "@/providers/AppProviders.";
+import "@rainbow-me/rainbowkit/styles.css";
+import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppProviders>{children}</AppProviders>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,50 @@
-export default function Home() {
+"use client";
+
+import { ConnectCard } from "@/features/auth/components/ConnectCard";
+import { WrongNetworkCard } from "@/features/auth/components/WrongNetworkCard";
+import { AuthState, useAuth } from "@/features/auth/providers/AuthProvider";
+import { Header } from "@/features/layouts/components/Header";
+import { PageContent, PageLayout } from "@/features/layouts/components/Layouts";
+import { TxCard } from "@/features/transactions/components/TxCard";
+import { useTransactionManager } from "@/features/transactions/providers/TransactionManagerProvider";
+import { SearchVaultFormCard } from "@/features/vaults/components/SearchVaultFormCard";
+import { VaultCard } from "@/features/vaults/components/VaultCard";
+
+const Home = () => {
+  const { transaction } = useTransactionManager();
+
+  if (transaction) {
+    return <TxCard {...transaction} />;
+  }
+
   return (
-    <div>
-      <main>
-        <h1>Morpho Vault</h1>
-      </main>
+    <div className="flex flex-col gap-[24px]">
+      <SearchVaultFormCard />
+      <VaultCard />
     </div>
+  );
+};
+
+const VIEWS: Record<AuthState, React.ComponentType> = {
+  connecting: ConnectCard,
+  disconnected: ConnectCard,
+  wrongNetwork: WrongNetworkCard,
+  connected: Home,
+};
+
+export default function RoutePage() {
+  const { isAuthenticated, authState } = useAuth();
+
+  const Component = VIEWS[authState];
+
+  return (
+    <>
+      <PageLayout>
+        {isAuthenticated && <Header />}
+        <PageContent>
+          <Component />
+        </PageContent>
+      </PageLayout>
+    </>
   );
 }
