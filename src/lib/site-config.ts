9fdbbc3a6cc32e25940572667547648208d9d270
Oscarmacieira <macieiraoscar@icloud.com>
2024-11-11 17:31:04 +0000

    feat(lib): added site-config

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -1,4 +1,6 @@
 export const siteConfig = {
   title: "Morpho Vault",
   description: "A simple dapp to withdraw all your funds from a Morpho Vault",
+  debounceTime: 500,
+  txConfirmations: 1,
 };
